b776e175802aa5c8a3997e628cedb79d719361cd
Alex Cummaudo <alexcu@me.com>
2016-07-22 12:12:10 +1000

    CONFIG: Update env.js to support Heroku
    Additionally make further changes to add both name and version
    numbering to the env config

--- env.config.js
+++ env.config.js
@@ -2,21 +2,27 @@ module.exports = {
   env: {
     options: {
       // Global Options
+      VERSION: require('./package.json').version,
     },
 
     development: {
-      API_URL  : 'http://' + require('ip').address() + ':3000/api',
-      DEV_MODE : 'true'
+      API_URL: 'http://' + require('ip').address() + ':3000/api',
+      ENV_NAME: 'development'
     },
 
     production: {
-      API_URL  : 'https://doubtfire.ict.swin.edu.au/api',
-      DEV_MODE : 'false'
+      API_URL: 'https://doubtfire.ict.swin.edu.au/api',
+      ENV_NAME: 'production'
     },
 
     docker: {
-      API_URL  : 'http://' + process.env.DOUBTFIRE_DOCKER_MACHINE_IP + ':3000/api',
-      DEV_MODE : 'true'
+      API_URL: 'http://' + process.env.DOUBTFIRE_DOCKER_MACHINE_IP + ':3000/api',
+      ENV_NAME: 'docker'
+    },
+
+    heroku: {
+      API_URL: '/api',
+      ENV_NAME: 'heroku'
     }
   }
 }
